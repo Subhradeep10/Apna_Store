13fd4f73cb033510f5b8015910b64931630fc5ec
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-07-13 21:02:06 +0530

    Auth screen init

---
+++ lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthScreeen extends StatefulWidget {
+  const AuthScreeen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreeen> createState() => _AuthScreeenState();
+}
+
+class _AuthScreeenState extends State<AuthScreeen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
