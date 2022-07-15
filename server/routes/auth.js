b52a624efa7d2f495c0fd348a659616924c15591
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-07-15 08:08:14 +0530

    Hashing added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const authRouter = express.Router();
+const bcrypt = require('bcryptjs');
 
 authRouter.post('/api/signup', async (req, res) => {
     try {
@@ -9,10 +10,12 @@ authRouter.post('/api/signup', async (req, res) => {
         if (existingUser) {
             return res.status(400).json({ error: 'User already exists' });
         }
+        const hashedPassword = await bcrypt.hash(password, 12);
+
         let user = new User({
             name,
             email,
-            password
+            password: hashedPassword,
         })
         user = await user.save();
         res.json(user);
