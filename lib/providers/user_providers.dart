2504653a934cb36fbfa543ca700a7f22ca54ddf8
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-07-17 21:16:42 +0530

    Signin init

---
+++ lib/providers/user_providers.dart
@@ -0,0 +1,19 @@
+import 'package:apna_store/models/user.dart';
+import 'package:flutter/material.dart';
+
+class UserProvider extends ChangeNotifier {
+  User _user = User(
+      id: '',
+      name: '',
+      email: '',
+      password: '',
+      address: '',
+      type: '',
+      token: '');
+
+  User get user => _user;
+  void setUser(String user) {
+    _user = User.fromJson(user);
+    notifyListeners();
+  }
+}
