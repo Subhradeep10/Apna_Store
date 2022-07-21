d7455b5961a4f85b8a533e1f20ac613f9254bd09
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-07-21 19:00:55 +0530

    Middleware init

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,13 @@ const jwt = require('jsonwebtoken');
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
+        if (!token) {
+            return res.status(401).json({ msg: 'No token, authorization denied' });
+        }
         const decoded = jwt.verify(token, 'secret');
+        if (!decoded) {
+            return res.status(401).json({ msg: 'Token is not valid' });
+        }
         req.user = decoded;
         next();
     } catch (err) {
